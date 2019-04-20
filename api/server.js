8abf1b46c9221583b3efc58437e4c21200a0bd1f
Nicolás Riquelme <nrriquelme@gmail.com>
2019-04-19 20:04:28 -0400

    Docker

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const DataHanlder = require('./DataHandler');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/process-data', DataHanlder);
 
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button color="danger">Danger!</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
